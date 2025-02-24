8ed106e18d985bc965c463e34bb3c7027a87af02
Aaron Blas <161629230+aorexe@users.noreply.github.com>
2025-02-24 08:29:52 -0500

    Update spring run point

--- src/main/java/com/server/ServerApplication.java
+++ src/main/java/com/server/ServerApplication.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class ServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
+        SpringApplication.run(ServerApplication.class, args);
     }
 }
 
